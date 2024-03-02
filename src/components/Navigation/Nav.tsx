7630c06bc762b7c55d1d27e68b6df68bed27614a
laughing-nerd <riturajpaul99@gmail.com>
2024-03-02 21:50:40 +0530

    feat: added login page

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -1,18 +1,11 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/RDaZw7aWRuv
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import Link from "next/link";
 import { Button } from "@/components/ui/button"
 import MenuToggle from "../theme/MenuToggle";
 
-
-
 export default function Nav() {
   
   return (
-    <div className="navbar flex flex-row justify-between items-center pt-2 px-9">
+    <div className="navbar flex flex-row justify-between items-center pt-2">
       <div className="text-primary text-3xl font-bold">Logo</div>
       
       <div className="buttons flex gap-2">

d3222619f73ac40f101e70b57ddb7003fe1091c7
GairikSharma <gairiksharma202@gmail.com>
2024-03-02 19:14:54 +0530

    landing page and typewritter effect

---
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { HeartPulse } from "lucide-react";
+import { data } from "../../CardData/CardData";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { motion } from "framer-motion";
+import { staggerVariants } from "@/framer/staggerVariants";
+
+const FeatureCard = () => {
+  useEffect(() => {
+    Aos.init({ duration: 550 });
+  }, []);
+  return (
+    <>
+      <div className=" cursor-pointer px-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {data.map((item, index) => (
+          <motion.div
+          key={index}
+          variants={staggerVariants}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{once: true}}
+          custom={index}
+          >
+            <Card className="w-full h-[180px] shadow-sm hover:shadow-md transition-all">
+              <CardHeader>
+                <div className="card-icon-wrapper bg-primary text-secondary w-10 h-10 flex items-center justify-center rounded-full">
+                  <HeartPulse />
+                </div>
+                <CardTitle className="border-b border-primary w-1/2 pb-2">
+                  {item.title}
+                </CardTitle>
+                <CardDescription>{item.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default FeatureCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import MenuToggle from "@/components/theme/MenuToggle";
+import FeatureCard from "@/components/FeatureCard/FeatureCard";
+import LandingPage from "@/components/LandingPage/LandingPage";
+import Nav from "@/components/Navigation/Nav";
+import TypeWritter from "@/components/TypeWritter/TypeWritter";
 import React from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full min-h-screen gap-4">
-      <div className="flex justify-end p-6">
-        <MenuToggle />
-      </div>
-      <h1 className="p-2 text-4xl font-bold  min-h-max h-[600px] flex justify-center items-center">
-        Hello
-      </h1>
+      
+      {/* <Nav /> */}
+      <LandingPage />
+      <TypeWritter />
     </div>
   );
 }

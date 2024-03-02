cd26a8ebc66f059e4f457fa86ae927d214492dda
Slutishboy <sohomsaha361@gmail.com>
2024-03-02 23:52:18 +0530

    map_intigrated

---
+++ src/components/DocList/DocList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DocList ():any{
+    
+    return (
+    <div className="flex flex-col w-full min-h-screen gap-4">
+    
+        
+    </div>);
+}
